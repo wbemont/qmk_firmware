e3d97c9e20dd937eae63e074ea939f78a242b5d1
Levi Portenier <leviport377@gmail.com>
2022-07-18 12:08:28 -0600

    Make sure USB hub re-initializes before entering bootloader mode

--- keyboards/system76/launch_2/launch_2.c
+++ keyboards/system76/launch_2/launch_2.c
@@ -222,3 +222,43 @@ void suspend_wakeup_init_kb(void) {
     rgb_matrix_set_suspend_state(false);
     suspend_wakeup_init_user();
 }
+
+void bootloader_jump(void) {
+
+    // Disable all peripherals on AT90USB646
+    UDCON = 1;
+    USBCON = (1<<FRZCLK);  // disable USB
+    UCSR1B = 0;
+    _delay_ms(5);
+
+    EIMSK  = 0;
+    PCICR  = 0;
+    SPCR   = 0;
+    ACSR   = 0;
+    EECR   = 0;
+    ADCSRA = 0;
+    TIMSK0 = 0;
+    TIMSK1 = 0;
+    TIMSK2 = 0;
+    TIMSK3 = 0;
+    UCSR1B = 0;
+    TWCR   = 0;
+    DDRA   = 0;
+    DDRB   = 0;
+    DDRC   = 0;
+    DDRD   = 0;
+    DDRE   = 0;
+    DDRF   = 0;
+    PORTA  = 0;
+    PORTB  = 0;
+    PORTC  = 0;
+    PORTD  = 0;
+    PORTE  = 0;
+    PORTF  = 0;
+
+    // initialize USB hub before jumping to bootloader
+    usb_mux_init();
+
+    // finally, jump to bootloader
+    asm volatile("jmp 0xFC00");
+}
