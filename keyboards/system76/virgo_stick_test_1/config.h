0ebdf2f55876f7c093e0772b3e3cccc4836bed19
Jeremy Soller <jackpot51@gmail.com>
2023-05-23 17:40:04 -0600

    Add virgo_stick_test_1 keyboard

---
+++ keyboards/system76/virgo_stick_test_1/config.h
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (C) 2023  System76
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "config_common.h"
+
+#define MATRIX_ROWS 1
+#define MATRIX_COLS 1
+
+#define MATRIX_ROW_PINS { F4 }
+#define MATRIX_COL_PINS { F5 }
+
+#define DIODE_DIRECTION COL2ROW
+
+// Set 0 if debouncing isn't needed
+#define DEBOUNCE 5
+
+// Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap
+#define LOCKING_SUPPORT_ENABLE
+
+// Locking resynchronize hack
+#define LOCKING_RESYNC_ENABLE
+
+#define PS2_CLOCK_PIN D2
+#define PS2_DATA_PIN D1
+
+#define PS2_INT_INIT()  do {    \
+    EICRA |= ((1<<ISC21) |      \
+              (0<<ISC20));      \
+} while (0)
+#define PS2_INT_ON()  do {      \
+    EIMSK |= (1<<INT2);         \
+} while (0)
+#define PS2_INT_OFF() do {      \
+    EIMSK &= ~(1<<INT2);        \
+} while (0)
+#define PS2_INT_VECT   INT2_vect
---
+++ keyboards/system76/virgo_stick_test_1/virgo_stick_test_1.h
@@ -0,0 +1,13 @@
+#ifndef VIRGO_STICK_TEST_1_H
+#define VIRGO_STICK_TEST_1_H
+
+#include "quantum.h"
+
+#define ___ KC_NO
+
+#define LAYOUT(K0) \
+  { \
+    { K0 }, \
+  }
+
+#endif // VIRGO_STICK_TEST_1_H
